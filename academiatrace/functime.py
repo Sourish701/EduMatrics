b00232dfb434d0b7623578834b2ff41569f1ffae
Sourish701 <duttasourish701@gmail.com>
2025-12-31 22:53:08 +0530

    modified:   academiatrace/functime.py

--- academiatrace/functime.py
+++ academiatrace/functime.py
@@ -1,20 +1,7 @@
 import time
-def get_clean_input():
-    while True:
-        user_input = input("Enter a number: ")
-        
-        if user_input.isdigit():
-            # If correct, we keep it and break
-            print(f"Success! You entered: {user_input}")
-            break
-        else:
-            # If wrong, wait a second so they see the error
-            print("Invalid entry! Try again...", end="", flush=True)
-            time.sleep(1)
-            
-            # THE TRICK:
-            # 1. Move up 1 line (to the 'Invalid entry' line) and clear it
-            # 2. Move up another line (to the user's input line) and clear it
-            print("\033[1A\033[K" + "\033[1A\033[K", end="", flush=True)
-
-get_clean_input()
+def flash_and_input(message,prompt):
+    print(message,end="",flush=True)
+    time.sleep(2)
+    print("\r"+" "*len(message)+"\r",end="",flush=True)
+    print("\033[1A\033[K" + "\033[1A\033[K", end="", flush=True)
+    return input(prompt)        
